b2cbcda0a697c7c394eb646ef363ba30a619c5e1
Elephant153 <sko03kim4@naver.com>
2022-06-01 15:23:24 +0900

    1
    1

--- POS_Project/src/Login/Menus.java
+++ POS_Project/src/Login/Menus.java
@@ -2,6 +2,12 @@ package Login;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +22,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.AncestorListener;
+import javax.swing.table.DefaultTableModel;
 
 public class Menus extends JPanel {
 	Menus() throws SQLException{
@@ -34,18 +42,19 @@ public class Menus extends JPanel {
 		MenuInfo menu_info = new MenuInfo();
 		ArrayList <String[]> menu_information = menu_info.getMenuInfo();
 		HashSet <String> category_text = new HashSet<String>();
-		category_text.add("ÀüÃ¼");
+		category_text.add("ì „ì²´");
 		for (String[] menuItem : menu_information) {
 			category_text.add(menuItem[1]);
 		}
 		String[] category = category_text.toArray(new String[0]);
 		Arrays.sort(category);
-		menus_sub_1_1.add(new JLabel("Ä«Å×°í¸®"));
+		menus_sub_1_1.add(new JLabel("ì¹´í…Œê³ ë¦¬"));
 		JComboBox categoryBox = new JComboBox(category);
+		
 		menus_sub_1_1.add(categoryBox);
 		
 		menus_sub_1_2.setLayout(new GridLayout(1,1));
-		String[] table_colum = {"ÄÚµå","Ä«Å×°í¸®","¸Ş´ºÀÌ¸§","±İ¾×"};
+		String[] table_colum = {"ì½”ë“œ","ì¹´í…Œê³ ë¦¬","ë©”ë‰´ì´ë¦„","ê¸ˆì•¡"};
 		String[][] table_row = new String[menu_information.size()][4];
 		for (int i=0; i<menu_information.size();i++) {
 			String[] getArray = new String[4];
@@ -54,10 +63,18 @@ public class Menus extends JPanel {
 				table_row[i][j] = getArray[j];
 			}
 		}
-		JTable table = new JTable(table_row,table_colum);
+		DefaultTableModel dtm = new DefaultTableModel(table_row,table_colum) {
+			public boolean isCellEditable(int i, int c) {
+                return false;
+            }
+		};
+		JTable table = new JTable(dtm);
 		JScrollPane scrollPane = new JScrollPane(table);
 		menus_sub_1_2.add(scrollPane);
 		
+		
+		
+		categoryBox.addActionListener(new categoryAction(table));
 		
 		menus_sub_1.setLayout(new BorderLayout());
 		menus_sub_1.add("North",menus_sub_1_1);
@@ -69,18 +86,30 @@ public class Menus extends JPanel {
 		menus_sub_2_1.setLayout(new GridLayout(4,2));
 		menus_sub_2_2.setLayout(new GridLayout(1,3));
 		
-		String[] menuInfoText = {"ÄÚµå", "Ä«Å×°í¸®", "¸Ş´º ÀÌ¸§", "±İ¾×"};
+		String[] menuInfoText = {"ì½”ë“œ","ì¹´í…Œê³ ë¦¬","ë©”ë‰´ì´ë¦„","ê¸ˆì•¡"};
 		JTextField[] menuInfoInput = new JTextField[4];
 		for (int i=0; i<menuInfoText.length; i++) {
 			menuInfoInput[i] = new JTextField();
 			menus_sub_2_1.add(new JLabel(menuInfoText[i]));
 			menus_sub_2_1.add(menuInfoInput[i]);
 		}
-		JButton[] menuInfoBtn = {new JButton("Ãß°¡"),new JButton("¼öÁ¤"),new JButton("»èÁ¦")};
+		menuInfoInput[0].setEnabled(false);
+		JButton[] menuInfoBtn = {new JButton("ï¿½ß°ï¿½"),new JButton("ï¿½ï¿½ï¿½ï¿½"),new JButton("ï¿½ï¿½ï¿½ï¿½")};
 		for (int i=0; i<menuInfoBtn.length; i++) {
 			menus_sub_2_2.add(menuInfoBtn[i]);
 		}
 		
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JTable jt = (JTable)e.getSource();
+				int row = jt.getSelectedRow();
+				menuInfoInput[0].setText((String)jt.getValueAt(row, 0));
+				menuInfoInput[1].setText((String)jt.getValueAt(row, 1));
+				menuInfoInput[2].setText((String)jt.getValueAt(row, 2));
+				menuInfoInput[3].setText((String)jt.getValueAt(row, 3));
+			}
+		});
 		menus_sub_2.setLayout(new BorderLayout());
 		menus_sub_2.add("Center",menus_sub_2_1);
 		menus_sub_2.add("South",menus_sub_2_2);
@@ -109,3 +138,44 @@ public class Menus extends JPanel {
 		}
 	}
 }
+	class categoryAction implements ActionListener{
+		JTable table;
+		DefaultTableModel dtm;
+		categoryAction(JTable table){
+			this.table = table;
+			this.dtm = (DefaultTableModel)this.table.getModel();
+		}
+		public void actionPerformed(ActionEvent e) {
+			JComboBox combo=(JComboBox)e.getSource();
+			String category_name = combo.getSelectedItem().toString();
+			SqlConnection sqlConn;
+			try {
+				String sql = null;
+				ResultSet rs = null;
+				sqlConn = new SqlConnection();
+				Connection conn = sqlConn.getConnection();
+				if (category_name != "ì „ì²´") {
+					sql = "select * from menu_info where mCategory=?";
+					PreparedStatement preState = conn.prepareStatement(sql);
+					preState.setString(1, category_name);
+					rs = preState.executeQuery();
+				}
+				else if(category_name.equals("ì „ì²´")){
+					sql = "select * from menu_info";
+					Statement st = conn.createStatement();
+					rs = st.executeQuery(sql);
+				}
+				dtm.setRowCount(0);
+				
+				while(rs.next()) {
+					String[] addingArray = new String[4];
+					addingArray[0] = Integer.toString(rs.getInt("mId"));
+					addingArray[1] = rs.getString("mCategory");
+					addingArray[2] = rs.getString("mMenu");
+					addingArray[3] = Integer.toString(rs.getInt("mPrice"));
+					dtm.addRow(addingArray);
+				}
+			} catch (SQLException e1) {}
+			
+		}
+	}
